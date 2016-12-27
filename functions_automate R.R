fba60576bce48d08c4c770a90fb9fb78e32d80cd
Sambid Kumar Pradhan <sambid9988@gmail.com>
2016-12-27 22:29:53 +0530

    Update functions_automate R.R

--- functions_automate R.R
+++ functions_automate R.R
@@ -77,5 +77,74 @@ for(i in names(ins_fact)){
 ####making the missing values unavailable
 for (i in seq_along(insurance_data)) set(insurance_data, i=which(is.na(insurance_data[[i]])), j=i, value="Unavailable")
 
+##Concordance
+
+Concordance = function(GLM.binomial) {
+  outcome_and_fitted_col = cbind(GLM.binomial$y, GLM.binomial$fitted.values)
+  # get a subset of outcomes where the event actually happened
+  ones = outcome_and_fitted_col[outcome_and_fitted_col[,1] == 1,]
+  # get a subset of outcomes where the event didn't actually happen
+  zeros = outcome_and_fitted_col[outcome_and_fitted_col[,1] == 0,]
+  # Equate the length of the event and non-event tables
+  if (length(ones[,1])>length(zeros[,1])) {ones = ones[1:length(zeros[,1]),]}
+  else {zeros = zeros[1:length(ones[,1]),]}
+  # Following will be c(ones_outcome, ones_fitted, zeros_outcome, zeros_fitted)
+  ones_and_zeros = data.frame(ones, zeros)
+  # initiate columns to store concordant, discordant, and tie pair evaluations
+  conc = rep(NA, length(ones_and_zeros[,1]))
+  disc = rep(NA, length(ones_and_zeros[,1]))
+  ties = rep(NA, length(ones_and_zeros[,1]))
+  for (i in 1:length(ones_and_zeros[,1])) {
+    # This tests for concordance
+    if (ones_and_zeros[i,2] > ones_and_zeros[i,4])
+    {conc[i] = 1
+    disc[i] = 0
+    ties[i] = 0}
+    # This tests for a tie
+    else if (ones_and_zeros[i,2] == ones_and_zeros[i,4])
+    {
+      conc[i] = 0
+      disc[i] = 0
+      ties[i] = 1
+    }
+    # This should catch discordant pairs.
+    else if (ones_and_zeros[i,2] < ones_and_zeros[i,4])
+    {
+      conc[i] = 0
+      disc[i] = 1
+      ties[i] = 0
+    }
+  }
+  # Here we save the various rates
+  conc_rate = mean(conc, na.rm=TRUE)
+  disc_rate = mean(disc, na.rm=TRUE)
+  tie_rate = mean(ties, na.rm=TRUE)
+  return(list(concordance=conc_rate, num_concordant=sum(conc), discordance=disc_rate, num_discordant=sum(disc), tie_rate=tie_rate,num_tied=sum(ties)))
+} 
+
+Concordance(lgOut)
+
+  LogRegData$Score <- predict(lgOut,LogRegData,type="response")
+##Lift chart
+  
+LogRegData$Score <- predict(lgOut,LogRegData,type="response")
 
 
+Pctl_tbl<-as.vector(quantile(LogRegData[,"Score"], probs=c(.10,.20, .30,.40,.50, .60,.70, .80,.90)))
+Pctl_tbl<-data.frame(c("P10","P20","P30","P40","P50","P60","P70","P80","P90"),Pctl_tbl)
+colnames(Pctl_tbl)<-c("quantiles","Score")
+
+
+
+LogRegData$Segment <-ifelse(LogRegData[,"Score"] > Pctl_tbl[Pctl_tbl$quantiles=="P90",2] , 10, 
+                            ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P80",2], 9, 
+                                   ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P70",2], 8,
+                                          ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P60",2],7,
+                                                 ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P50",2],6,
+                                                        ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P40",2],5,
+                                                               ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P30",2],4,
+                                                                      ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P20",2],3,ifelse(LogRegData[,"Score"] >= Pctl_tbl[Pctl_tbl$quantiles=="P10",2],                                                  2, 1)))))))))
+
+LiftChartData<-data.frame(CrossTable(LogRegData$Default_On_Payment,LogRegData$Segment) )
+LiftChartData <- LiftChartData[LiftChartData$prop.col.x==1,c("t.Freq","prop.col.y")]
+LiftChartData
